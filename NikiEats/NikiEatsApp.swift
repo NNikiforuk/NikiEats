6f4bb800b5c01738fbf1caf24a1552406fcfac37
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-24 18:02:29 +0200

    Initial Commit

---
+++ NikiEats/NikiEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikiEatsApp.swift
+//  NikiEats
+//
+//  Created by Natalia Nikiforuk on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NikiEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
