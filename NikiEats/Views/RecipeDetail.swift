03823851e8deca440570d04c8ab1dd366ef9a335
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-28 15:29:53 +0100

    Basic list for recipes

---
+++ NikiEats/Views/RecipeDetail.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetail.swift
+//  NikiEats
+//
+//  Created by Natalia Nikiforuk on 28/10/2024.
+//
+
+import SwiftUI
+
+struct RecipeDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetail()
+}
